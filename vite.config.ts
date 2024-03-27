e0f250a69582f4bddcccc52bf968da269a154ee0
scseong <goidtv@naver.com>
2024-03-27 16:57:09 +0900

    chore: 절대경로 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: [
+      { find: '@apis', replacement: './apis' },
+      { find: '@assets', replacement: './assets' },
+      { find: '@components', replacement: './components' },
+      { find: '@hooks', replacement: './hooks' },
+      { find: '@layouts', replacement: './layouts' },
+      { find: '@pages', replacement: './pages' },
+      { find: '@shared', replacement: './shared' },
+      { find: '@styles', replacement: './styles' },
+      { find: '@types', replacement: './types' },
+    ],
+  },
+});

01b8b3fefd03a57fed6d63cf02b679ee6b74fee1
scseong <goidtv@naver.com>
2024-04-02 21:54:52 +0900

    feat: 로딩 컴포넌트 적용

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,10 +1,28 @@
+import { useEffect, useState } from 'react';
 import { Outlet } from 'react-router-dom';
 import { Footer, Header, Main, Sidebar } from './layouts';
 import { GlobalStyle } from '@styles/GlobalStyle';
 import styled from 'styled-components';
 import { AuthContextProvider } from '@context/AuthContext';
+import { auth } from '@apis/firebase';
+import LoadingIndicator from '@components/common/LoadingIndicator';
 
 export default function Root() {
+  const [isLoading, setLoading] = useState(true);
+
+  const init = async () => {
+    await auth.authStateReady();
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    init();
+  }, []);
+
+  if (isLoading) {
+    return <LoadingIndicator />;
+  }
+
   return (
     <StLayoutContainer>
       <AuthContextProvider>

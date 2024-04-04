05da600307c0f5be40e937f88a0764a423c04960
scseong <goidtv@naver.com>
2024-04-04 22:24:01 +0900

    chore: NotoSansKR 웹 폰트 적용

--- src/styles/GlobalStyle.ts
+++ src/styles/GlobalStyle.ts
@@ -1,4 +1,7 @@
 import { createGlobalStyle, css } from 'styled-components';
+import NotoSansKRBold from '@assets/fonts/NotoSansKR-Bold.woff2';
+import NotoSansKRSemiBold from '@assets/fonts/NotoSansKR-SemiBold.woff2';
+import NotoSansKRRegular from '@assets/fonts/NotoSansKR-Regular.woff2';
 
 export const GlobalStyle = createGlobalStyle`
   *{margin:0;padding:0;font:inherit;color:inherit;}
@@ -10,6 +13,25 @@ export const GlobalStyle = createGlobalStyle`
   menu, ol, li {list-style:none}
   a {text-decoration:none}
   table {border-collapse:collapse;border-spacing:0}
+  *, button, textarea, a, input {
+    font-family: 'NotoSansKR', sans-serif;
+  }
+
+  @font-face {
+    font-family: 'NotoSansKR';
+    src: url(${NotoSansKRRegular}) format('woff2');
+    font-weight: 400;
+  }
+  @font-face {
+    font-family: 'NotoSansKR';
+    src: url(${NotoSansKRSemiBold}) format('woff2');
+    font-weight: 600;
+  } 
+  @font-face {
+    font-family: 'NotoSansKR';
+    src: url(${NotoSansKRBold}) format('woff2');
+    font-weight: 700;
+  } 
 `;
 
 export const blindStyle = css`

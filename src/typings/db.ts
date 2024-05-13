bcfef9aaa1b6163c3913be532d2011a7a6c24644
scseong <goidtv@naver.com>
2024-05-13 18:48:49 +0900

    chore: 아티클 Mock 데이터 생성 및 타입 정의

--- src/typings/db.ts
+++ src/typings/db.ts
@@ -15,3 +15,17 @@ export type ChildCategory = {
   fullSlug: string;
   order: number;
 };
+
+export type Article = {
+  id: number;
+  publishedAt: string;
+  link: string;
+  categories: { id: number; name: string; slug: string }[];
+  author: string;
+  title: string;
+  content: string;
+  thumbnail: string;
+  tags: string[];
+  bookmark: boolean;
+  isRead: boolean;
+};
